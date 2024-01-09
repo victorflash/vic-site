b16b066ed97e34338bb713e48bc199e094a767f1
Vittorio Cassese <victorflash@gmail.com>
2024-01-09 18:26:23 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,7 +66,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'Vic Site',
+        title: 'Vic Site & Blog',
         logo: {
           alt: 'Vic Site Logo',
           src: 'img/logo.svg',
